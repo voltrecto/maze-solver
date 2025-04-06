e68bf6c7686ae8a02941e1863cf3770d18af927d
voltrecto <voltairerecto@gmail.com>
2025-04-06 11:58:13 -0500

    Add Maze

---
+++ maze.py
@@ -0,0 +1,45 @@
+from window import Cell
+import time
+
+class Maze:
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win,
+    ):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self.cells = []
+        self._create_cells()
+    
+    def _create_cells(self):
+        for i in range(self.num_cols):
+            cols = []
+            for j in range(self.num_rows):
+                cols.append(Cell(self.win))
+            self.cells.append(cols)
+        for i in range(self.num_cols):
+            for j in range(self.num_rows):
+                self._draw_cell(i, j)
+    
+    def _draw_cell(self, i, j):
+        x1 = self.x1 + i * self.cell_size_x
+        y1 = self.y1 + i * self.cell_size_y
+        x2 = x1 + self.cell_size_x
+        y2 = y1 + self.cell_size_y
+        self.cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
+
+    def _animate(self):
+        self.win.redraw()
+        time.sleep(0.05)
